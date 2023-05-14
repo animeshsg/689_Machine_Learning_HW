7b5abdbdfe5431a3f15e3b14a5af62590364d359
animeshsg <asg.animesh@gmail.com>
2023-05-14 14:04:59 -0400

    [HW4] Final

--- HW4/code/ae.py
+++ HW4/code/ae.py
@@ -1,17 +1,457 @@
+import numpy as np
 import torch
-import torch.nn as nn
 import torch.optim as optim
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+import copy
 
 
-class denoiser:
-    def __init__(self,D,K):
-        pass
+# denoiser class is linearAE class 
+class linear_AE(nn.Module):
+    def __repr__(self):
+        return {"model":"LinearAE","K":self.K,"lr":self.lr,"epoch":self.epochs}
+    
+    def __init__(self, dim,K,epoch=1000,lr=0.001):
+        super(linear_AE, self).__init__()
+        self.model=nn.Sequential(
+            nn.Linear(dim, K),
+            nn.Linear(K,dim)
+            )
+        self.lr=lr
+        self.epochs=epoch
+        self.criterion=nn.MSELoss(reduction="mean")
+        self.K=K
 
-    def reconstruct(self,X):
-        pass
-        
-    def loss(self,X,Xprime):
-        pass
-        
-    def fit(self,X,R,lr,maxiter):
-        pass
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+    def reconstruct(self,x,W,V):
+        self.model[0].weight.data=W
+        self.model[1].weight.data=V
+        x=self.model(x)
+        return x
+    
+    def loss(self,X,X_hat):
+        return self.criterion(X,X_hat).item()
+    
+    
+    def fit(self,X,R):
+        train_loss_list=[]
+        optimizer=optim.Adam(self.parameters(),lr=self.lr)
+        for epoch in range(self.epochs+1):
+            train_loss = 0.0
+            self.model.train()
+            optimizer.zero_grad()
+            output = self.model(X)
+            loss = self.criterion(output, R)
+
+            loss.backward()
+            optimizer.step()
+            train_loss_list.append(loss)
+            #if(epoch%100==0):print('Epoch [{}/{}], Train Loss: {:.9f}'.format(epoch, self.epochs, loss))
+        #self.saver(self.model.state_dict())
+        return loss.item()
+
+    def test(self,X,R):
+        self.model.eval()
+        with torch.no_grad():
+            output=self.model(X)
+            loss=self.criterion(output,R)
+            return loss
+
+    def print_param(self):
+        for param in self.parameters():
+            print(f'parameter shape:{param.shape}')
+            #print(f'parameter value:{param.data}')
+    
+    @staticmethod
+    def plot_loss(X,X_hat):
+        time=len(X)
+        plt.plot(range(1, time+1), X.cpu().detach().numpy(), label='Noisy Data')
+        plt.plot(range(1, time+1), X_hat.cpu().detach().numpy(), label='Reconstructed Data')
+        plt.xlabel('Time')
+        plt.ylabel('ECG')
+        plt.title('Noisy Data vs. Reconstructed Data')
+        plt.legend()
+        plt.show()
+
+    def saver(self,best_weights,PATH="./models"):
+        name=''.join(f"{key}{val}" for key, val in self.__repr__().items())
+        path=PATH+"/"+name
+        torch.save(best_weights,path)
+
+class tanh_AE(nn.Module):
+    def __repr__(self):
+        return {"model":"tanh","K":self.K,"lr":self.lr,"epoch":self.epochs}
+    
+    def __init__(self, dim,K,epoch=1000,lr=0.001):
+        super(tanh_AE, self).__init__()
+        self.model=nn.Sequential(
+            nn.Linear(dim, K),
+            nn.Tanh(),
+            nn.Linear(K,dim)
+            )
+        self.lr=lr
+        self.epochs=epoch
+        self.criterion=nn.MSELoss(reduction="mean")
+        self.K=K
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+    def reconstruct(self,x,W,V):
+        self.model[0].weight.data=W
+        self.model[1].weight.data=V
+        x=self.model(x)
+        return x
+    
+    def loss(self,X,X_hat):
+        return self.criterion(X,X_hat).item()
+    
+    
+    def fit(self,X,R):
+        train_loss_list=[]
+        optimizer=optim.Adam(self.parameters(),lr=self.lr)
+        for epoch in range(self.epochs+1):
+            train_loss = 0.0
+            self.model.train()
+            optimizer.zero_grad()
+            output = self.model(X)
+            loss = self.criterion(output, R)
+
+            loss.backward()
+            optimizer.step()
+            train_loss_list.append(loss)
+            #if(epoch%100==0):print('Epoch [{}/{}], Train Loss: {:.9f}'.format(epoch, self.epochs, loss))
+        #self.saver(self.model.state_dict())
+        return loss.item()
+
+    def test(self,X,R):
+        self.model.eval()
+        with torch.no_grad():
+            output=self.model(X)
+            loss=self.criterion(output,R)
+            return loss
+
+    def print_param(self):
+        for param in self.parameters():
+            print(f'parameter shape:{param.shape}')
+            #print(f'parameter value:{param.data}')
+    
+    @staticmethod
+    def plot_loss(X,X_hat):
+        time=len(X)
+        plt.plot(range(1, time+1), X.cpu().detach().numpy(), label='Noisy Data')
+        plt.plot(range(1, time+1), X_hat.cpu().detach().numpy(), label='Reconstructed Data')
+        plt.xlabel('Time')
+        plt.ylabel('ECG')
+        plt.title('Noisy Data vs. Reconstructed Data')
+        plt.legend()
+        plt.show()
+
+    def saver(self,best_weights,PATH="./models"):
+        name=''.join(f"{key}{val}" for key, val in self.__repr__().items())
+        path=PATH+"/"+name
+        torch.save(best_weights,path)
+
+class Relu_AE(nn.Module):
+    def __repr__(self):
+        return {"model":"relu","K":self.K,"lr":self.lr,"epoch":self.epochs}
+    
+    def __init__(self, dim,K,epoch=1000,lr=0.001):
+        super(Relu_AE, self).__init__()
+        self.model=nn.Sequential(
+            nn.Linear(dim, K),
+            nn.ReLU(),
+            nn.Linear(K,dim)
+            )
+        self.lr=lr
+        self.epochs=epoch
+        self.criterion=nn.MSELoss(reduction="mean")
+        self.K=K
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+    def reconstruct(self,x,W,V):
+        self.model[0].weight.data=W
+        self.model[1].weight.data=V
+        x=self.model(x)
+        return x
+    
+    def loss(self,X,X_hat):
+        return self.criterion(X,X_hat).item()
+    
+    
+    def fit(self,X,R):
+        train_loss_list=[]
+        optimizer=optim.Adam(self.parameters(),lr=self.lr)
+        for epoch in range(self.epochs+1):
+            train_loss = 0.0
+            self.model.train()
+            optimizer.zero_grad()
+            output = self.model(X)
+            loss = self.criterion(output, R)
+
+            loss.backward()
+            optimizer.step()
+            train_loss_list.append(loss)
+            #if(epoch%100==0):print('Epoch [{}/{}], Train Loss: {:.9f}'.format(epoch, self.epochs, loss))
+        #self.saver(self.model.state_dict())
+        return loss.item()
+
+    def test(self,X,R):
+        self.model.eval()
+        with torch.no_grad():
+            output=self.model(X)
+            loss=self.criterion(output,R)
+            return loss
+
+    def print_param(self):
+        for param in self.parameters():
+            print(f'parameter shape:{param.shape}')
+            #print(f'parameter value:{param.data}')
+    
+    @staticmethod
+    def plot_loss(X,X_hat):
+        time=len(X)
+        plt.plot(range(1, time+1), X.cpu().detach().numpy(), label='Noisy Data')
+        plt.plot(range(1, time+1), X_hat.cpu().detach().numpy(), label='Reconstructed Data')
+        plt.xlabel('Time')
+        plt.ylabel('ECG')
+        plt.title('Noisy Data vs. Reconstructed Data')
+        plt.legend()
+        plt.show()
+
+    def saver(self,best_weights,PATH="./models"):
+        name=''.join(f"{key}{val}" for key, val in self.__repr__().items())
+        path=PATH+"/"+name
+        torch.save(best_weights,path)
+
+class leakyRelu_AE(nn.Module):
+    def __repr__(self):
+        return {"model":"Leakyrelu","K":self.K,"lr":self.lr,"epoch":self.epochs}
+    
+    def __init__(self, dim,K,epoch=1000,lr=0.001):
+        super(leakyRelu_AE, self).__init__()
+        self.model=nn.Sequential(
+            nn.Linear(dim, K),
+            nn.LeakyReLU(),
+            nn.Linear(K,dim)
+            )
+        self.lr=lr
+        self.epochs=epoch
+        self.criterion=nn.MSELoss(reduction="mean")
+        self.K=K
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+    def reconstruct(self,x,W,V):
+        self.model[0].weight.data=W
+        self.model[1].weight.data=V
+        x=self.model(x)
+        return x
+    
+    def loss(self,X,X_hat):
+        return self.criterion(X,X_hat).item()
+    
+    
+    def fit(self,X,R):
+        train_loss_list=[]
+        optimizer=optim.Adam(self.parameters(),lr=self.lr)
+        for epoch in range(self.epochs+1):
+            train_loss = 0.0
+            self.model.train()
+            optimizer.zero_grad()
+            output = self.model(X)
+            loss = self.criterion(output, R)
+
+            loss.backward()
+            optimizer.step()
+            train_loss_list.append(loss)
+            #if(epoch%100==0):print('Epoch [{}/{}], Train Loss: {:.9f}'.format(epoch, self.epochs, loss))
+        #self.saver(self.model.state_dict())
+        return loss.item()
+
+    def test(self,X,R):
+        self.model.eval()
+        with torch.no_grad():
+            output=self.model(X)
+            loss=self.criterion(output,R)
+            return loss
+
+    def print_param(self):
+        for param in self.parameters():
+            print(f'parameter shape:{param.shape}')
+            #print(f'parameter value:{param.data}')
+    
+    @staticmethod
+    def plot_loss(X,X_hat):
+        time=len(X)
+        plt.plot(range(1, time+1), X.cpu().detach().numpy(), label='Noisy Data')
+        plt.plot(range(1, time+1), X_hat.cpu().detach().numpy(), label='Reconstructed Data')
+        plt.xlabel('Time')
+        plt.ylabel('ECG')
+        plt.title('Noisy Data vs. Reconstructed Data')
+        plt.legend()
+        plt.show()
+
+    def saver(self,best_weights,PATH="./models"):
+        name=''.join(f"{key}{val}" for key, val in self.__repr__().items())
+        path=PATH+"/"+name
+        torch.save(best_weights,path)
+
+class mlpRelu_AE(nn.Module):
+    def __repr__(self):
+        return {"model":"relu","K":self.K,"lr":self.lr,"epoch":self.epochs}
+    
+    def __init__(self, dim,K,epoch=1000,lr=0.001):
+        super(mlpRelu_AE, self).__init__()
+        self.model=nn.Sequential(
+            nn.Linear(dim, K),
+            nn.ReLU(),
+            nn.Linear(K,256),
+            nn.ReLU(),
+            nn.Linear(256,dim)
+            )
+        self.lr=lr
+        self.epochs=epoch
+        self.criterion=nn.MSELoss(reduction="mean")
+        self.K=K
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+    def reconstruct(self,x,W,V):
+        self.model[0].weight.data=W
+        self.model[1].weight.data=V
+        x=self.model(x)
+        return x
+    
+    def loss(self,X,X_hat):
+        return self.criterion(X,X_hat).item()
+    
+    
+    def fit(self,X,R):
+        train_loss_list=[]
+        optimizer=optim.Adam(self.parameters(),lr=self.lr)
+        for epoch in range(self.epochs+1):
+            train_loss = 0.0
+            self.model.train()
+            optimizer.zero_grad()
+            output = self.model(X)
+            loss = self.criterion(output, R)
+
+            loss.backward()
+            optimizer.step()
+            train_loss_list.append(loss)
+            #if(epoch%100==0):print('Epoch [{}/{}], Train Loss: {:.9f}'.format(epoch, self.epochs, loss))
+        #self.saver(self.model.state_dict())
+        return loss.item()
+
+    def test(self,X,R):
+        self.model.eval()
+        with torch.no_grad():
+            output=self.model(X)
+            loss=self.criterion(output,R)
+            return loss
+
+    def print_param(self):
+        for param in self.parameters():
+            print(f'parameter shape:{param.shape}')
+            #print(f'parameter value:{param.data}')
+    
+    @staticmethod
+    def plot_loss(X,X_hat):
+        time=len(X)
+        plt.plot(range(1, time+1), X.cpu().detach().numpy(), label='Noisy Data')
+        plt.plot(range(1, time+1), X_hat.cpu().detach().numpy(), label='Reconstructed Data')
+        plt.xlabel('Time')
+        plt.ylabel('ECG')
+        plt.title('Noisy Data vs. Reconstructed Data')
+        plt.legend()
+        plt.show()
+
+    def saver(self,best_weights,PATH="./models"):
+        name=''.join(f"{key}{val}" for key, val in self.__repr__().items())
+        path=PATH+"/"+name
+        torch.save(best_weights,path)
+# write an RNN class with all its methods
+class RNNAE(nn.Module):
+    def __repr__(self):
+        return {"model":"RNN","HS":self.hidden_size,"NL":self.num_layers}
+    
+    def __init__(self, input_size, hidden_size, num_layers,epoch=1000,lr=0.001):
+        super(RNNAE, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.epochs=epoch
+        self.lr=lr
+        self.criterion=nn.MSELoss()
+
+        # RNN architecture
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True,nonlinearity="relu")
+        self.fc = nn.Linear(hidden_size, input_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, self.hidden_size).to(device)
+        out, _ = self.rnn(x, h0)
+        out = self.fc(out)
+        return out
+    
+    def loss(self,X,X_hat):
+        return self.criterion(X,X_hat).item()
+    
+    
+    def fit(self,X,R):
+        train_loss_list=[]
+        optimizer=optim.Adam(self.parameters(),lr=self.lr)
+        for epoch in range(self.epochs+1):
+            train_loss = 0.0
+            self.train()
+            optimizer.zero_grad()
+            output = self.forward(X)
+            loss = self.criterion(output, R)
+            loss.backward()
+            optimizer.step()
+            train_loss_list.append(loss)
+            if(epoch%100==0):print('Epoch [{}/{}], Train Loss: {:.9f}'.format(epoch, self.epochs, loss))
+        #self.saver(self.model.state_dict())
+        return loss.item()
+
+    def test(self,X,R):
+        self.eval()
+        with torch.no_grad():
+            output=self.model(X)
+            loss=self.criterion(output,R)
+            return loss
+
+    def print_param(self):
+        for param in self.parameters():
+            print(f'parameter shape:{param.shape}')
+            #print(f'parameter value:{param.data}')
+    
+    @staticmethod
+    def plot_loss(X,X_hat):
+        time=len(X)
+        plt.plot(range(1, time+1), X.cpu().detach().numpy(), label='Noisy Data')
+        plt.plot(range(1, time+1), X_hat.cpu().detach().numpy(), label='Reconstructed Data')
+        plt.xlabel('Time')
+        plt.ylabel('ECG')
+        plt.title('Noisy Data vs. Reconstructed Data')
+        plt.legend()
+        plt.show()
+
+    def saver(self,best_weights,PATH="./models"):
+        name=''.join(f"{key}{val}" for key, val in self.__repr__().items())
+        path=PATH+"/"+name
+        torch.save(best_weights,path)
--- HW4/code/ae_experiments.py
+++ HW4/code/ae_experiments.py
@@ -2,14 +2,80 @@ import numpy as np
 import torch
 import torch.nn as nn
 import  matplotlib.pyplot as plt
-import ae
+from ae import linear_AE,Relu_AE,leakyRelu_AE,tanh_AE,mlpRelu_AE,RNNAE
+from sklearn.model_selection import train_test_split
 
+np.random.seed(17)
+torch.manual_seed(17)
+torch.set_default_dtype(torch.float)
+device = torch.device('mps' if torch.backends.mps.is_available() else 'mps')
 
 data       = np.load("../data/ecg_data.npz")
-Xtr        = data["Xtr"]       #Clean train
-Xtr_noise  = data["Xtr_noise"] #Noisy train
-Xte_noise  = data["Xte_noise"] #Noisy test
+Xtr        = torch.tensor(data["Xtr"]).type(torch.float).to(device)    #Clean train
+Xtr_noise  = torch.tensor(data["Xtr_noise"]).type(torch.float).to(device)  #Noisy train
+Xte_noise  = torch.tensor(data["Xte_noise"]).type(torch.float).to(device)  #Noisy test
 
 params = np.load("../data/ecg_params.npz")
-W = torch.FloatTensor(params["W"]) #Decoder parameters
-V = torch.FloatTensor(params["V"]) #Encoder parameters
+W = torch.FloatTensor(params["W"]).type(torch.float).to(device)  #Decoder parameters
+V = torch.FloatTensor(params["V"]).type(torch.float).to(device)  #Encoder parameters
+
+#a
+model=linear_AE(100,5,lr=0.001).to(device)
+X_hat=model.reconstruct(Xte_noise[:5],V,W)
+for i in range(5):
+    model.plot_loss(Xte_noise[i],X_hat[i])
+
+#b
+X_hat=model.reconstruct(Xtr_noise[:5],V,W)
+bloss=model.loss(Xtr_noise[:5],X_hat)
+print("b:",bloss)
+
+#c
+model1=linear_AE(100,10,lr=0.01,epoch=2000).to(device)
+loss=model1.fit(Xtr,Xtr)
+print("c1:",loss)
+x_pred_noisy=model1.forward(Xtr_noise)
+print("c2:",model1.loss(Xtr,x_pred_noisy))
+
+#d
+model1=linear_AE(100,10,lr=0.01,epoch=2000).to(device)
+loss=model1.fit(Xtr_noise,Xtr)
+print("d:",loss)
+
+#f
+K=[k for k in range(10,150,5)]
+train_loss_list=[]
+test_loss_list=[]
+best_k=0
+best_loss=np.inf
+best_model=None
+X_train, X_val, R_train, R_val = train_test_split(Xtr_noise, Xtr, test_size=0.2)
+for k in K:
+    model=linear_AE(100,k,lr=0.001,epoch=1500).to(device)
+    train_loss=model.fit(X_train,R_train)
+    train_loss_list.append(train_loss)
+    test_loss=model.test(X_val,R_val)
+    test_loss_list.append(test_loss)
+    if test_loss<best_loss:
+        best_k=k
+        best_model=model
+        best_loss=test_loss
+    print(f'K:{k} Train Loss:{train_loss}   Test Loss:{test_loss}')
+
+print(f'Best K:{best_k}   Best Loss:{best_loss}')
+
+test_loss_lists=[x.cpu().detach().numpy() for x in test_loss_list]
+plt.plot(K, train_loss_list, label='train loss')
+plt.plot(K, test_loss_lists, label='test loss')
+plt.xlabel('K')
+plt.ylabel('loss')
+plt.title('K Vs loss (Linear Model)')
+plt.legend()
+plt.show()
+
+#h
+best_model=mlpRelu_AE(100,120,lr=0.001,epoch=1500).to(device)
+train_loss=best_model.fit(Xtr_noise,Xtr)
+X_pred=best_model.forward(Xte_noise)
+for i in range(5):
+    best_model.plot_loss(Xte_noise[i],X_pred[i])
